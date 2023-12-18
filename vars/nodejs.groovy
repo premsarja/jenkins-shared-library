19c52aa7736e8177d77d5793a3906d63b836def8
premsarja <premsagar@prodot.in>
2023-12-18 13:35:00 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,17 +1,24 @@
     // @Library('roboshop@main') _
+    def lintchecks() {
+            sh "echo Installing JSLint"
+            sh "npm install jslint"
+            sh "echo Starting lint checks**********"
+            sh "node_modules/jslint/bin/jslint.js server.js || true"
+            sh "echo Lint checks completed"
+
+    } 
+  
     def call() {
     pipeline {
         agent any 
            stages {
             stage('Lint Checks') {
                 steps {                
-                        sh "echo Installing JSLint"
-                        sh "npm install jslint"
-                        sh "echo Starting lint checks**********"
-                        sh "node_modules/jslint/bin/jslint.js server.js || true"
-                        sh "echo Lint checks completed"
-                    }
+                    script(
+                        lintchecks()
+                    )
                 }
+            }
             stage('Generating Artifacts') {
                 steps {
                     sh "echo Generating Artifacts"
