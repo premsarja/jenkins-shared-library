392666f0cdc0d493ad1bb271383e14264cb7cb94
premsarja <premsagar@prodot.in>
2023-12-18 11:38:15 +0530

    done

---
+++ vars/node.groovy
@@ -0,0 +1,8 @@
+lintchecks() {
+       sh "echo Installing JSLint"
+       sh "npm install jslint"
+       sh "echo Starting lint checks**********"
+       sh "node_modules/jslint/bin/jslint.js server.js || true"
+       sh "echo Lint checks completed"
+
+}
