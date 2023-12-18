75ded410f74975e06d4b165d56f7a673dcd41bf0
premsarja <premsagar@prodot.in>
2023-12-18 14:54:16 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@
 def lintchecks() {
     sh "echo Installing JSLint"
     sh "npm install jslint"
-    sh echo Starting lint checks********** $(COMPONENT)
+    sh echo Starting lint checks********** ${COMPONENT}
     sh "node_modules/jslint/bin/jslint.js server.js || true"
     sh "echo Lint checks completed"
 
