dd3ec7840d445a9023d78926d65b86bdbd0ebeb1
premsarja <premsagar@prodot.in>
2023-12-21 15:52:43 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -11,12 +11,24 @@ def lintchecks() {
     def call() {
     pipeline {
         agent any 
+        environment{
+            SONAR_URL= "172.31.89.159:9000"
+            SONAR_CRED =credentials('SONAR_CRED')
+        }
            stages {
             stage('Lint Checks') {
                 steps {                
                     script{
                         lintchecks()
                     }
+                }
+            }
+
+            stage('sonar check'){
+                steps{
+                    sh '''
+                    sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED_USR} -Dsonar-password=${SONAR_CRED_USR}
+                    '''
                 }
             }
             stage('Generating Artifacts') {
