f0859fe91440743b8ff2b7e0c6f6642ecd42ccb9
premsarja <premsagar@prodot.in>
2023-12-23 16:23:48 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,4 +1,5 @@
     // @Library('roboshop@main') _
+ // this is for nodejs lint checks   
 def lintchecks() {
     sh "echo Installing JSLint"
     sh "npm install jslint"
@@ -25,7 +26,8 @@ def lintchecks() {
                 }
                 steps{
                     sh "env"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"             
+                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
+                    sh "qualitygate.sh"             
                 }
             }
             stage('Generating Artifacts') {
