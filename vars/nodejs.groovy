0779829ab8ef30eb6a717496d2a71d4c7e9d6453
premsarja <premsagar@prodot.in>
2023-12-23 16:26:11 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -27,7 +27,7 @@ def lintchecks() {
                 steps{
                     sh "env"
                     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
-                    sh "qualitygate.sh"             
+                    sh " bash qualitygate.sh"             
                 }
             }
             stage('Generating Artifacts') {
