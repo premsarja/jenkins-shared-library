f9626ac033759832fbe1683d77c5524ae17cb3c1
premsarja <premsagar@prodot.in>
2023-12-27 07:24:02 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -16,12 +16,18 @@ def lintchecks() {
                     }
                 }
             }
+            stage('sonar check'){
+                environment{
+                        SONAR_URL="172.31.89.159"
+                        SONAR_CRED=credentials('SONAR_CRED')
+                    }    
+                }
                 steps{
                     sh "env"
                     sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
-                    sh " bash qualitygate.sh || true"  
-                }
+                    sh " bash qualitygate.sh || true"             
 
+                }
             stage('Generating Artifacts') {
                 steps {
                     sh "echo Generating Artifacts"
