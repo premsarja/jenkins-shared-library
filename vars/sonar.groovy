45cbf5e54166a4578234118732a8445793c370bd
premsarja <premsagar@prodot.in>
2023-12-27 21:56:17 +0530

    done

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,4 +1,4 @@
-def sonar(){
+def sonarcheck(){
 steps{
   sh "env"
   sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
