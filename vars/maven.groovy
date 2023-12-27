953a1d112a57ba93d618a65e95af96890fb05341
premsarja <premsagar@prodot.in>
2023-12-27 07:29:27 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -2,39 +2,36 @@ def lintchecks() {
     sh "echo Starting lint checks********** ${COMPONENT}"
     sh "mvn checkstyle:check || true"
     sh "echo Lint checks completed for ${COMPONENT}"
+}
 
-    } 
-  
-    def call() {
-    pipeline {
-        agent any 
-           stages {
-            stage('Lint Checks') {
-                steps {                
-                    script{
-                        lintchecks()
-                    }
-                }
-            }
-            stage("sonar check"){
-                environment{
-                        SONAR_URL="172.31.89.159"
-                        SONAR_CRED=credentials('SONAR_CRED')
-                    }    
-                }
-                steps{
-                    sh "env"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
-                    sh " bash qualitygate.sh || true"             
-
-                }
-            stage('Generating Artifacts') {
-                steps {
-                    sh "echo Generating Artifacts"
-                    sh "mvn clean package"
+pipeline {
+    agent any 
+    stages {
+        stage('Lint Checks') {
+            steps {                
+                script {
+                    lintchecks()
                 }
             }
         }
-    
+        
+        stage("Sonar Check") {
+            environment {
+                SONAR_URL = "172.31.89.159"
+                SONAR_CRED = credentials('SONAR_CRED')
+            }
+            steps {
+                sh "env"
+                sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED}"
+                sh "bash qualitygate.sh || true"             
+            }
+        }
+        
+        stage('Generating Artifacts') {
+            steps {
+                sh "echo Generating Artifacts"
+                sh "mvn clean package"
+            }
+        }
     }
-}   
+}
