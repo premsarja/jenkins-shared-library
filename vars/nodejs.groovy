449dedcaa38903f7af92266d80dbe423fe284f1e
premsarja <premsagar@prodot.in>
2023-12-27 21:47:03 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -9,34 +9,32 @@ def lintchecks() {
 
     } 
   
-    def call() {
-    pipeline {
-        agent any 
-           stages {
-            stage('Lint Checks') {
-                steps {                
-                        lintchecks()
+def call() {
+pipeline {
+     agent any 
+        stages {
+         stage('Lint Checks') {
+             steps {                
+                     lintchecks()
+             }
+         }
+         stage('sonar check'){
+             environment{
+                     SONAR_URL="172.31.89.159"
+                     SONAR_CRED=credentials('SONAR_CRED')
+             }
+             steps{
+                script{
+                    sonar()
                 }
-            }
-
-            stage('sonar check'){
-                environment{
-                        SONAR_URL="172.31.89.159"
-                        SONAR_CRED=credentials('SONAR_CRED')
-                }
-                steps{
-                    sh "env"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
-                    sh " bash qualitygate.sh || true"             
-                }
-            }
-            stage('Generating Artifacts') {
-                steps {
-                    sh "echo Generating Artifacts"
-                    sh "npm install"
-                }
+             }
+         }
+         stage('Generating Artifacts') {
+             steps {
+                 sh "echo Generating Artifacts"
+                 sh "npm install"
             }
         }
-    
-    }
-}   
+      }
+   }
+}  
