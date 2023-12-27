b4d253ee99963505ce7d7086acdcede48549ea26
premsarja <premsagar@prodot.in>
2023-12-27 15:10:36 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -30,7 +30,7 @@ pipeline {
         stage("Sonar Check") {
             steps {
                 sh "env"
-                sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_CRED}"
+                sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f""
                 sh "bash qualitygate.sh || true"             
             }
         }
