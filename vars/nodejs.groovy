8f6fbfa8ae736e08075c0a613c2611fb213e0fe4
premsarja <premsagar@prodot.in>
2023-12-28 15:31:37 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -14,7 +14,7 @@ def sonarcheck(){
   sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
   sh " bash qualitygate.sh || true"             
   }
-}  
+
 
 def call() {
 pipeline {
