28de684caddd024ba385c412d4b6461dcfcc1cbd
premsarja <premsagar@prodot.in>
2023-12-28 22:26:39 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -10,7 +10,7 @@ def lintchecks() {
 
 def sonarcheck() {
     sh "env"
-    sh "sonar-scanner -Dsonar.host.url=http://172.31.89.159:9000/ -Dsonar.sources=. -Dsonar.projectKey=${env.COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+    sh "sonar-scanner -Dsonar.host.url=${SONAR_URL} -Dsonar.sources=. -Dsonar.projectKey=${env.COMPONENT} -Dsonar.login=admin -Dsonar.password=${SONAR_CRED}"
     sh "bash qualitygate.sh || true"             
 }
 
@@ -18,7 +18,7 @@ pipeline {
     agent any 
     environment {
         SONAR_URL = "172.31.89.159"
-        SONAR_CRED = credentials('SONAR_CRED')
+        SONAR_CRED = credentials('password')
         // COMPONENT = "${env.COMPONENT}" // Replace with your component name or pass it externally
     }
     stages {
