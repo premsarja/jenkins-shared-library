40b577de9642e9480b675bf683ef9ad8e6faa776
premsarja <premsagar@prodot.in>
2023-12-30 15:33:31 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,12 +5,14 @@ def lintchecks() {
     sh "npm install jslint"
     sh "echo Starting lint checks********** ${env.COMPONENT}"
     sh "node_modules/jslint/bin/jslint.js server.js || true"
-    sh "echo Lint checks completed"
+    sh "echo Lint checks completed for ${env.COMPONENT}"
 }
 
 def sonarcheck() {
     sh "env"
     sh "sonar-scanner -X -Dsonar.host.url=${SONAR_URL} -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=83b03e3e897fb76ef13f25c264b75e81ed043ed"
+    sh "curl https://gitlab.com/thecloudcareers/opensource/-/blob/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
+    sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PWD} ${SONAR_URL} ${COMPONENT}"
     sh "bash qualitygate.sh || true"
 } 
 
@@ -19,7 +21,6 @@ pipeline {
     agent any 
     environment {
         SONAR_URL = "172.31.89.159"
-         SONAR_CRED = "admin"
         // COMPONENT = "${env.COMPONENT}" // Replace with your component name or pass it externally
     }
     stages {
