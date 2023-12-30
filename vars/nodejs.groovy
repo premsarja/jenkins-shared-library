a00b46a3f1b0c13a56060a3fde5e0b73d678499e
premsarja <premsagar@prodot.in>
2023-12-30 17:08:20 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -12,38 +12,37 @@ def sonarcheck() {
     sh "env"
     sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=83b03e3e897fb76ef13f25c264b75e81ed043ed"
     sh "bash qualitygate.sh || true"
-} 
+}
 
-def call(){
-pipeline {
-    agent any 
-    environment {
-        SONAR_URL = "172.31.89.159"
-        SONAR_CRED = credentials('SONAR_CRED')
-    }    
-   // COMPONENT = "${env.COMPONENT}" // Replace with your component name or pass it externally
-    }
-    stages {
-        stage('Lint Checks') {
-            steps {
-                script {
-                    lintchecks()
+def call() {
+    pipeline {
+        agent any 
+        environment {
+            SONAR_URL = "172.31.89.159"
+            SONAR_CRED = credentials('SONAR_CRED')
+        }    
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+                }
+            }
+            stage('Sonar Check') {
+                steps {
+                    script {
+                        sonarcheck()
+                    }
+                }
+            }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Generating Artifacts"
+                    sh "npm install"
+                    // Add any other necessary commands for generating artifacts
                 }
             }
         }
-        stage('Sonar Check') {
-            steps {
-                script {
-                    sonarcheck()
-                }
-            }
-        }
-        stage('Generating Artifacts') {
-            steps {
-                sh "echo Generating Artifacts"
-                sh "npm install"
-                // Add any other necessary commands for generating artifacts
-            }
-        }
-    }
-}  
+    }  
+}
