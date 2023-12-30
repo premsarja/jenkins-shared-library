479e5a8b7943fc2219f97ba529a2b09eb734ab4c
premsarja <premsagar@prodot.in>
2023-12-30 15:34:12 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -13,7 +13,6 @@ def sonarcheck() {
     sh "sonar-scanner -X -Dsonar.host.url=${SONAR_URL} -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=83b03e3e897fb76ef13f25c264b75e81ed043ed"
     sh "curl https://gitlab.com/thecloudcareers/opensource/-/blob/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh"
     sh "bash quality-gate.sh ${SONAR_CRED_USR} ${SONAR_CRED_PWD} ${SONAR_URL} ${COMPONENT}"
-    sh "bash qualitygate.sh || true"
 } 
 
 def call(){
