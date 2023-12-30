597bd2781574f8df1c732dd232f4953858b40992
premsarja <premsagar@prodot.in>
2023-12-30 15:40:09 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -20,6 +20,10 @@ pipeline {
     agent any 
     environment {
         SONAR_URL = "172.31.89.159"
+        SONAR_CRED = credentials('SONAR_CRED')
+} 
+
+        
         // COMPONENT = "${env.COMPONENT}" // Replace with your component name or pass it externally
     }
     stages {
@@ -45,4 +49,4 @@ pipeline {
             }
         }
     }
-  }  }
+  }  
