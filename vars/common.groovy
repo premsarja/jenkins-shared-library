6a6a24dd06b917f9e1b8d4bc8d3f946e0d24427c
premsarja <premsagar@prodot.in>
2024-01-01 12:46:06 +0530

    done

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,5 +1,5 @@
 def sonarcheck() {
     sh "env"
-    sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. $ARGS -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+    sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ $ARGS -Dsonar.sources=.  -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
     sh "bash qualitygate.sh || true"
 }
