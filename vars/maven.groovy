f828abcb3ec4be7610d17e13a6466af8b222251a
premsarja <premsagar@prodot.in>
2024-01-01 16:03:55 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,20 +1,20 @@
 def lintChecks() {
-    sh "echo 'Starting lint checks********** ${env.COMPONENT}'"
+    sh "echo 'Starting lint checks********** ${COMPONENT}'"
     sh "mvn checkstyle:check || true"
-    sh "echo 'Lint checks completed for ${env.COMPONENT}'"
+    sh "echo 'Lint checks completed for ${COMPONENT}'"
 }
-
+def call(){
 pipeline {
     agent any 
     environment {
         SONAR_URL = "172.31.89.159"
-        // SONAR_CRED = credentials('SONAR_CRED')
+        //SONAR_CRED = credentials('SONAR_CRED')
     }
     stages {
         stage('Lint Checks') {
             steps {
                 script {
-                    lintChecks()
+                    lintChecks() // Replace "YourComponentName" with your actual component name
                 }
             }
         }
@@ -22,7 +22,7 @@ pipeline {
         stage('code compile') {
             steps {
                 sh '''
-                echo generating artifacts for ${env.COMPONENT}
+                echo generating artifacts for $(COMPONENT)
                 mvn clean compile
                 '''
             }
@@ -31,37 +31,35 @@ pipeline {
         stage("Sonar Check") {
             steps {
                 script {
-                    env.ARGS = "-Dsonar.java.binaries=target/"
-                    // common.sonarcheck() - There is no 'common' object defined
-                    // Add commands related to SonarQube using env.ARGS or any other variable you prefer
-                    // Example: sh "mvn sonar:sonar ${env.ARGS}"
+                    env.ARGS="-Dsonar.java.binaries=target/"
+                    common.sonarcheck()
+                    // Add commands related to SonarQube using sonarArgs
+                    // Example: sh "mvn sonar:sonar ${sonarArgs}"
                 }
             }
         }
-
-        stage('parallel') {
-            parallel {
-                stage('unit testing') {
-                    steps {
-                        sh "echo starting unit testing"
-                        sh "echo unit test completed"
-                    }
-                }     
-                stage('integration testing') {
-                    steps {
-                        sh "echo starting integration testing"
-                        sh "echo integration test completed"
-                    }
-                }      
-                stage('functional testing') {
-                    steps {
-                        sh "echo starting functional testing"
-                        sh "echo functional test completed"
-                    }
-                }
+     stage('parallel'){
+        parallel{
+        stage('unit testing'){
+            steps{
+                sh "echo starting unit testing"
+                sh "echo unit test completed"
+            }
+        }     
+         stage('integration testing'){
+            steps{
+                sh "echo starting unit testing"
+                sh "echo unit test completed"
+            }
+        }      
+          stage('functional testing'){
+            steps{
+                sh "echo starting unit testing"
+                sh "echo unit test completed"
             }
         }
 
+    }
         stage('Generating Artifacts') {
             steps {
                 sh "echo 'Generating Artifacts'"
@@ -69,4 +67,5 @@ pipeline {
             }
         }
     }
-}
+  }
+ }
