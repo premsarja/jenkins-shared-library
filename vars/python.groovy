1a4ab18bee9d4edbe5b92bc12fb4ee4d7d2921cb
premsarja <premsagar@prodot.in>
2024-01-01 10:28:39 +0530

    done

--- vars/python.groovy
+++ vars/python.groovy
@@ -18,12 +18,11 @@ def call() {
             stage("sonar-checks"){
                 environment{
                         SONAR_URL="172.31.89.159"
-                        SONAR_CRED=credentials('SONAR_CRED')
+                        // SONAR_CRED=credentials('SONAR_CRED')
                 }
                 steps{
                     sh "env"
-                    sh "sonar-scanner -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=e66657d47a6ff847cd4ff38fbc26b74a4a9d359f"
-                    sh " bash qualitygate.sh || true"      
+                    sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
                 }
             }
                 
