96899af612c759317ea345a4f8692327304249f5
premsarja <premsagar@prodot.in>
2024-01-02 07:01:34 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -4,70 +4,22 @@ def lintChecks() {
     sh "echo Lint checks completed for ${env.COMPONENT}"
 }
 
-pipeline {
-    agent any
-    environment {
-        SONAR_URL = "172.31.89.159"
-        //SONAR_CRED = credentials('SONAR_CRED')
-        // COMPONENT = "YourComponentName" // Define COMPONENT if not already set
-    }
-    stages {
-        stage('Lint Checks') {
-            steps {
-                script {
-                    lintChecks()
-                }
-            }
-        }
-
-        stage('code compile') {
-            steps {
-                sh "echo generating artifacts for ${env.COMPONENT}"
-                sh "mvn clean compile || true"
-            }
-        }
-
-        stage("Sonar Check") {
-            steps {
-                script {
-                    env.ARGS = "-Dsonar.java.binaries=target/"
-                    common.sonarcheck()
-                    // Add commands related to SonarQube using env.ARGS or any other variable you prefer
-                    // Example: sh "mvn sonar:sonar ${env.ARGS}"
-                }
-            }
-        }
-
-        stage('Tests') {
-            parallel {
-                stage('unit testing') {
-                    steps {
-                        sh "echo starting unit testing"
-                        sh "echo unit test completed"
-                    }
-                }
-
-                stage('integration testing') {
-                    steps {
-                        sh "echo starting integration testing"
-                        sh "echo integration test completed"
-                    }
-                }
-
-                stage('functional testing') {
-                    steps {
-                        sh "echo starting functional testing"
-                        sh "echo functional test completed"
+def call(){
+    pipeline{
+        agent any
+        environment(
+            SONAR_URL = "172.31.89.159"
+        )
+        
+        stages{
+            stage('lint checks'){
+                steps{
+                    script{
+                        linkchecks()
                     }
                 }
             }
         }
 
-        stage('Generating Artifacts') {
-            steps {
-                sh "echo 'Generating Artifacts'"
-                sh "mvn clean package"
-            }
-        }
     }
 }
