5040b588fb0676708f800eccebf821de755db5c8
premsarja <premsagar@prodot.in>
2024-01-02 07:26:57 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -7,7 +7,7 @@ def call() {
     pipeline {
         agent any 
         environment {
-            SONAR_URL = "172.31.81.131"
+            SONAR_URL = "172.31.89.159"
             // SONAR_CRED  = credentials('SONAR_CRED')
         }
         stages {
