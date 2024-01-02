4aae7af737c2f338426011f7fc3d5bf3ffeba032
premsarja <premsagar@prodot.in>
2024-01-02 07:23:49 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -3,21 +3,57 @@ def lintChecks() {
     sh "mvn checkstyle:check || true"
     sh "echo Lint checks completed for ${env.COMPONENT}"
 }
-def call(){  
+def call() {
     pipeline {
-       agent any
+        agent any 
         environment {
-            SONAR_URL = "172.31.89.159"
+            SONAR_URL = "172.31.81.131"
+            SONAR_CRED  = credentials('SONAR_CRED')
         }
-    }    
-    stages {
-        stage('lint checks') {
-            steps {
-                script {
-                    lintChecks() // Corrected function name to call lintChecks()
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintChecks()
+                    }
+                }
+            }
+            stage('Code Compile') {
+                steps {
+                    sh "echo Generating Artifiacts for $COMPONENT"
+                    sh "mvn clean compile"
+                    }
+                }
+            stage('Sonar Checks') {
+                steps {
+                    script {
+                            env.ARGS="-Dsonar.java.binaries=target/"
+                            common.sonarChecks()
+                        }
+                    }
+                }
+            stage('Test Cases') {
+                parallel {
+                    stage('Unit Testing') {
+                        steps {
+                            sh "echo Starting Unit Testing"
+                            sh "echo Unit Testing Completed"
+                        }
+                    }
+                    stage('Integration Testing') {
+                        steps {
+                            sh "echo Starting Integration Testing"
+                            sh "echo Integration Testing Completed"
+                        }
+                    }
+                    stage('Functional Testing') {
+                        steps {
+                            sh "echo Starting Functional Testing"
+                            sh "echo Functional Testing Completed"
+                        }
+                    }
                 }
             }
         }
-        // Add more stages if needed
     }
-}
+}            
