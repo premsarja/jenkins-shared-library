dbedb64076a4a8eca35448175d4f3db658c7dc6c
premsarja <premsagar@prodot.in>
2024-01-03 09:37:13 +0530

    done

--- vars/python.groovy
+++ vars/python.groovy
@@ -21,10 +21,8 @@ def call() {
                         // SONAR_CRED=credentials('SONAR_CRED')
                 }
                 steps{
-                    script{
-                        common.sonarcheck()
-                    }
-                    // sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
+                    sh "env"
+                    sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
                 }
             }
                 
