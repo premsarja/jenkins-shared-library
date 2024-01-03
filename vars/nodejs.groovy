bc24096ca819b09d221627778cbc57aad567164d
premsarja <premsagar@prodot.in>
2024-01-03 23:03:49 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -49,26 +49,22 @@ def call() {
             }
             stage('Check The Release') {
                 when {
-                    expression {  env.TAG_NAME != null }
+                    expression { env.TAG_NAME != null }
                 }
                 steps {
                     script {
-                        echo "TAG_NAME: ${env.TAG_NAME}" // Print TAG_NAME value for debugging
-                        def output = sh(script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true", returnStdout: true).trim()
-                        env.UPLOAD_STATUS = output ?: ""
-                        echo "UPLOAD_STATUS: ${env.UPLOAD_STATUS}" // Print UPLOAD_STATUS for debugging
-                        println env.UPLOAD_STATUS // Print UPLOAD_STATUS content for debugging}
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+                        print UPLOAD_STATUS
                     }
                 }
             }
             stage('Generating Artifacts') {
                 when {
-                        expression { env.TAG_NAME != 0 }
-                        expression { env.UPLOAD_STATUS == "" }
-                    }
-                      
+                    expression { env.TAG_NAME != null }
+                    expression { env.UPLOAD_STATUS == "" }
+                }
                 steps {
-                    sh "echo Generating Artifacts..."
+                    sh "echo Generating Artifiacts...."
                     sh "npm install"
                     sh "zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js"
                     sh "ls -ltr"
@@ -76,19 +72,17 @@ def call() {
                 }
             stage('Uploading Artifacts') {
                 when {
-                        expression { env.TAG_NAME != null }
-                        expression { env.UPLOAD_STATUS == "" }
-
-                    }
-
+                    expression { env.TAG_NAME != null }
+                    expression { env.UPLOAD_STATUS == "" }
+                }
                 steps {
                     sh '''
-                        echo Uploading ${COMPONENT} artifact to Nexus...
+                        echo Uploading ${COMPONENT} artifact to nexus
                         curl -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-                        echo Uploading ${COMPONENT} artifact to Nexus is completed
+                        echo Uploading ${COMPONENT} artifact to nexus is completed
                     ''' 
+                    }
                 }
             }
         }
     }
-}
