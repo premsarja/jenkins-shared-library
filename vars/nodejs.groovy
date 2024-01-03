ae0dbe29c528e90c3f5cafeafacf1857610aee05
premsarja <premsagar@prodot.in>
2024-01-03 07:51:45 +0530

    done

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -65,10 +65,11 @@ def call() {
                 }
                 steps {
                     sh '''
-                    echo uploading Artifacts
-                    curl -v -u '${NEXUS_CRED_USR}:${NEXUS_CRED_PSW}' --upload-file '${COMPONENT}-${TAG_NAME}.zip' 'http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip' 
-                    '''
-                }
+                        echo Uploading ${COMPONENT} artifact to nexus
+                        curl -v -u ${NEXUS_CRED_USR}:${NEXUS_CRED_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                        echo Uploading ${COMPONENT} artifact to nexus is completed
+                       '''                 
+                }   }
             }
     }   }
 }
