dedc5d92308cba6d1cc2ee32bf6f3dc76e29369a
premsarja <premsagar@prodot.in>
2024-01-04 12:11:02 +0530

    done

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -52,10 +52,47 @@ def call() {
                             sh "echo Functional Testing Completed"
                         }
                     }
+            stage('Check The Release') {
+                when {
+                    expression {  env.TAG_NAME != null }
                 }
-                
+                steps {
+                    script {
+                        echo "TAG_NAME: ${env.TAG_NAME}" // Print TAG_NAME value for debugging
+                        env.UPLOAD_STATUS = sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT}/ | grep ${COMPONENT}-${TAG_NAME}.zip || true").trim()
+                        echo "UPLOAD_STATUS: ${env.UPLOAD_STATUS}" // Print UPLOAD_STATUS for debugging
+                        println env.UPLOAD_STATUS // Print UPLOAD_STATUS content for debugging}
+                    }
+                }
             }
-            
+            stage('Generating Artifacts') {
+                when {
+                        expression { env.TAG_NAME != null }
+                        expression { env.UPLOAD_STATUS == '' }
+                    }
+                      
+                steps {
+                    sh "echo Generating Artifacts..."
+                    sh "npm install"
+                    sh "zip ${COMPONENT}-${TAG_NAME}.zip node_modules server.js"
+                    sh "ls -ltr"
+                    }
+                }
+                 stage('Uploading Artifacts') {
+                     when {
+                         expression { env.TAG_NAME != null }
+                        expression { env.UPLOAD_STATUS == '' }
+
+                    }
+
+                  steps {
+                    sh '''
+                        echo Uploading ${COMPONENT} artifact to Nexus...
+                        curl -v admin:password --upload-file ${COMPONENT}-${TAG_NAME}.zip http://${NEXUS_URL}:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+                        echo Uploading ${COMPONENT} artifact to Nexus is completed
+                    ''' 
+                }
+            }
         }
     }
-}            
+}
