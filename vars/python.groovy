1556314ff6249417c853b7735414711361c0fd38
premsarja <premsagar@prodot.in>
2024-01-04 12:40:08 +0530

    done

--- vars/python.groovy
+++ vars/python.groovy
@@ -25,8 +25,45 @@ def call() {
                     sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
                 }
             }
-                
-
+            stage('Test Cases') {
+                parallel {
+                    stage('Unit Testing') {
+                        steps {
+                            sh "echo Starting Unit Testing"
+                            sh "echo Unit Testing Completed"
+                        }
+                    }
+                    stage('Integration Testing') {
+                        steps {
+                            sh "echo Starting Integration Testing"
+                            sh "echo Integration Testing Completed"
+                        }
+                    }
+                    stage('Functional Testing') {
+                        steps {
+                            sh "echo Starting Functional Testing"
+                            sh "echo Functional Testing Completed"
+                        }
+                    }
+                }
+            }
+            stage('Check The Release') {
+                when {
+                    expression { env.TAG_NAME != null }
+                }
+                steps {
+                    script {
+                        env.UPLOAD_STATUS=sh(returnStdout: true, script: "curl -L -s http://${NEXUS_URL}:8081/service/rest/repository/browse/${COMPONENT} | grep ${COMPONENT}-${TAG_NAME}.zip || true")
+                        print UPLOAD_STATUS
+                    }
+                }
+            }
+            stage('Generating Artifacts') {
+                steps {
+                    sh "echo Artifact Generation Complete"
+                    
+                }
+            }
         }
     }
-}
+}                        
