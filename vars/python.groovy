2f87369406972673d8a528d381901cbee7ff0969
premsarja <premsagar@prodot.in>
2024-01-04 14:08:24 +0530

    done

--- vars/python.groovy
+++ vars/python.groovy
@@ -4,8 +4,14 @@ def lintchecks() {
     sh " echo linkchecks completed for ${COMPONENT}"
 }
 
+def call() {
     pipeline{
         agent any
+        environment{
+                        SONAR_URL="172.31.89.159"
+                        NEXUS_URL="172.31.60.99"   
+                        // SONAR_CRED=credentials('SONAR_CRED')
+        }
         stages{
             stage("lint charges"){
                 steps{
@@ -15,11 +21,6 @@ def lintchecks() {
                 }
             }
             stage("sonar-checks"){
-                environment{
-                        SONAR_URL="172.31.89.159"
-                        NEXUS_URL="172.31.60.99"   
-                        // SONAR_CRED=credentials('SONAR_CRED')
-                    }
                 steps{
                     sh "env"
                     sh "sonar-scanner -X -Dsonar.host.url=http://${SONAR_URL}:9000/ -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=admin -Dsonar.password=password"
@@ -65,4 +66,4 @@ def lintchecks() {
                 }
             }
         }
-    
+    }
