669dc9a122a3fa90229ccf1077108b914bf04cc2
premsarja <premsagar@prodot.in>
2024-01-08 11:06:20 +0530

    done

--- vars/common.groovy
+++ vars/common.groovy
@@ -69,7 +69,7 @@ def artifacts() {
             withCredentials([usernamePassword(credentialsId: 'NEXUS_CRED', passwordVariable: 'NEXUS_PSW', usernameVariable: 'NEXUS_USR')]) {
                 sh "echo Uploading ${COMPONENT} artifact to Nexus"
                 sh "pwd"
-                sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.22.243:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
+                sh "curl -v -u ${NEXUS_USR}:${NEXUS_PSW} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://172.31.60.99:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip"
                 sh "pwd"
                 sh "echo Uploading ${COMPONENT} artifact to Nexus is completed"
             }
